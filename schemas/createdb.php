10f120d42aed82fcf7d5af0d0e32096de4404c1f
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-24 15:55:51 +0300

    Improved script that programmatically bootstrap the database

--- schemas/createdb.php
+++ schemas/createdb.php
@@ -1,225 +1,255 @@
 <?php
-
 /**
  * Programmatically bootstrap the database
  *
+ * @var \Phalcon\Db\AdapterInterface $connection
  */
 
-use \Phalcon\Db\Column,
-    \Phalcon\Db\Index,
-    \Phalcon\Db\Reference;
-
-$config = new Phalcon\Config\Adapter\Ini(__DIR__ . '/../app/config/config.ini');
-
-$dbclass = sprintf('\Phalcon\Db\Adapter\Pdo\%s', $config->database->adapter);
-$connection = new $dbclass(array(
-	"host" => $config->database->host,
-	"username" => $config->database->username,
-	"password" => $config->database->password,
-	"dbname" => $config->database->name
-));
+use Phalcon\Exception;
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Config\Adapter\Ini as IniConfig;
+use Phalcon\Config;
 
 try {
-	$connection->begin();
+    $configFile = __DIR__ . '/../app/config/config.ini';
+    if (!is_file($configFile)) {
+        throw new Exception(
+            sprintf('Unable to read config file located at %s.', $configFile)
+        );
+    }
 
-	$connection->createTable(
-		"companies",
-		null,
-		array(
-			"columns" => array(
-				new Column("id", array(
-					'type' => Column::TYPE_INTEGER,
-					'size' => 10,
-					'unsigned' => TRUE,
-					'notNull' => TRUE,
-					'autoIncrement' => TRUE,
-				)),
-				new Column("name", array(
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 70,
-					'notNull' => TRUE,
-				)),
-				new Column("telephone", array(
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 30,
-					'notNull' => TRUE,
-				)),
-				new Column("address", array(
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 40,
-					'notNull' => TRUE,
-				)),
-				new Column("city", array(
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 40,
-					'notNull' => TRUE,
-				))
-			),
-			"indexes" => array(
-				new Index("PRIMARY", array("id")),
-			)
-		)
-	);
+    $config = new IniConfig($configFile);
 
-	$connection->execute("INSERT INTO `companies` VALUES (1,'Acme','31566564','Address','Hello'),(2,'Acme Inc','+44 564612345','Guildhall, PO Box 270, London','London')");
+    /** @var \Phalcon\Config $config */
+    $config = $config->get('database');
 
-	$connection->createTable(
-		"contact",
-		null,
-		[
-			"columns" => [
-				new Column("id", [
-					'type' => Column::TYPE_INTEGER,
-					'size' => 10,
-					'unsigned' => TRUE,
-					'notNull' => TRUE,
-					'autoIncrement' => TRUE,
-				]),
-				new Column("name", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 70,
-					'notNull' => TRUE,
-				]),
-				new Column("email", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 70,
-					'notNull' => TRUE,
-				]),
-				new Column("comments", [
-					'type' => Column::TYPE_TEXT,
-					'notNull' => TRUE,
-				]),
-				new Column("created_at", [
-					'type' => Column::TYPE_DATETIME,
-				]),
-			],
-			"indexes" => [
-				new Index("PRIMARY", array("id")),
-			]
-		]
-	);
+    if (!$config instanceof Config) {
+        throw new Exception('Unable to read database config.');
+    }
 
-	$connection->createTable(
-		"product_types",
-		null,
-		[
-			"columns" => [
-				new Column("id", [
-					'type' => Column::TYPE_INTEGER,
-					'size' => 10,
-					'unsigned' => TRUE,
-					'notNull' => TRUE,
-					'autoIncrement' => TRUE,
-				]),
-				new Column("name", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 70,
-					'notNull' => TRUE,
-				]),
-			],
-			"indexes" => [
-				new Index("PRIMARY", array("id")),
-			]
-		]
-	);
+    $dbClass = sprintf('\Phalcon\Db\Adapter\Pdo\%s', $config->get('adapter', 'MySql'));
 
-	$connection->execute("INSERT INTO `product_types` VALUES (5,'Vegetables'),(6,'Fruits')");
+    if (!class_exists($dbClass)) {
+        throw new Exception(
+            sprintf('PDO adapter "%s" not found.', $dbClass)
+        );
+    }
 
-	$connection->createTable(
-		"products",
-		null,
-		[
-			"columns" => [
-				new Column("id", [
-					'type' => Column::TYPE_INTEGER,
-					'size' => 10,
-					'unsigned' => TRUE,
-					'notNull' => TRUE,
-					'autoIncrement' => TRUE,
-				]),
-				new Column("product_types_id", [
-					'type' => Column::TYPE_INTEGER,
-					'size' => 10,
-					'unsigned' => TRUE,
-					'notNull' => TRUE,
-				]),
-				new Column("name", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 70,
-					'notNull' => TRUE,
-				]),
-				new Column("price", [
-					'type' => Column::TYPE_DECIMAL,
-					'size' => 16,
-					'scale' => 2,
-					'notNull' => TRUE,
-				]),
-				new Column("active", [
-					'type' => Column::TYPE_CHAR,
-					'size' => 1,
-				]),
-			],
-			"indexes" => [
-				new Index("PRIMARY", array("id")),
-			]
-		]
-	);
+    $dbConfig = $config->toArray();
+    unset($dbConfig['adapter']);
 
-	$connection->execute("INSERT INTO `products` VALUES (1,5,'Artichoke','10.50','Y'),(2,5,'Bell pepper','10.40','Y'),(3,5,'Cauliflower','20.10','Y'),(4,5,'Chinese cabbage','15.50','Y'),(5,5,'Malabar spinach','7.50','Y'),(6,5,'Onion','3.50','Y'),(7,5,'Peanut','4.50','Y')");
+    $connection = new $dbClass($dbConfig);
 
-	$connection->createTable(
-		"users",
-		null,
-		[
-			"columns" => [
-				new Column("id", [
-					'type' => Column::TYPE_INTEGER,
-					'size' => 10,
-					'unsigned' => TRUE,
-					'notNull' => TRUE,
-					'autoIncrement' => TRUE,
-				]),
-				new Column("username", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 32,
-					'notNull' => TRUE,
-				]),
-				new Column("password", [
-					'type' => Column::TYPE_CHAR,
-					'size' => 40,
-					'notNull' => TRUE,
-				]),
-				new Column("name", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 120,
-					'notNull' => TRUE,
-				]),
-				new Column("email", [
-					'type' => Column::TYPE_VARCHAR,
-					'size' => 70,
-					'notNull' => TRUE,
-				]),
-				new Column("created_at", [
-					'type' => Column::TYPE_DATETIME,
-					'notNull' => TRUE,
-				]),
-				new Column("active", [
-					'type' => Column::TYPE_CHAR,
-					'size' => 1,
-					'notNull' => TRUE,
-				]),
-			],
-			"indexes" => [
-				new Index("PRIMARY", array("id")),
-			]
-		]
-	);
+    $connection->begin();
 
-	$connection->execute("INSERT INTO users VALUES (1,'demo', 'c0bd96dc7ea4ec56741a4e07f6ce98012814d853','Phalcon Demo','demo@phalconphp.com','2012-04-10 20:53:03','Y')");
+    $connection->createTable(
+        'companies',
+        null,
+        [
+            'columns' => [
+                new Column('id', [
+                    'type'          => Column::TYPE_INTEGER,
+                    'size'          => 10,
+                    'unsigned'      => true,
+                    'notNull'       => true,
+                    'autoIncrement' => true
+                ]),
+                new Column('name', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 70,
+                    'notNull' => true
+                ]),
+                new Column('telephone', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 30,
+                    'notNull' => true
+                ]),
+                new Column('address', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 40,
+                    'notNull' => true
+                ]),
+                new Column('city', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 40,
+                    'notNull' => true
+                ])
+            ],
+            "indexes" => [
+                new Index('PRIMARY', ["id"], 'PRIMARY')
+            ]
+        ]
+    );
 
-	$connection->commit();
+    $connection->execute("INSERT INTO `companies` VALUES (1,'Acme','31566564','Address','Hello'),(2,'Acme Inc','+44 564612345','Guildhall, PO Box 270, London','London')");
 
-} catch (Exception $e) {
-	$connection->rollback();
-	echo $e->getTraceAsString();
+    $connection->createTable(
+        'contact',
+        null,
+        [
+            'columns' => [
+                new Column('id', [
+                    'type'          => Column::TYPE_INTEGER,
+                    'size'          => 10,
+                    'unsigned'      => true,
+                    'notNull'       => true,
+                    'autoIncrement' => true,
+                ]),
+                new Column('name', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 70,
+                    'notNull' => true,
+                ]),
+                new Column('email', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 70,
+                    'notNull' => true,
+                ]),
+                new Column('comments', [
+                    'type'    => Column::TYPE_TEXT,
+                    'notNull' => true,
+                ]),
+                new Column('created_at', [
+                    'type'    => Column::TYPE_TIMESTAMP,
+                    'notNull' => true,
+                    'default' => 'CURRENT_TIMESTAMP',
+                ]),
+            ],
+            'indexes' => [
+                new Index('PRIMARY', ['id'], 'PRIMARY'),
+            ]
+        ]
+    );
+
+    $connection->createTable(
+        'product_types',
+        null,
+        [
+            'columns' => [
+                new Column('id', [
+                    'type'          => Column::TYPE_INTEGER,
+                    'size'          => 10,
+                    'unsigned'      => true,
+                    'notNull'       => true,
+                    'autoIncrement' => true
+                ]),
+                new Column('name', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 70,
+                    'notNull' => true
+                ]),
+            ],
+            'indexes' => [
+                new Index('PRIMARY', ['id'], 'PRIMARY')
+            ]
+        ]
+    );
+
+    $connection->execute("INSERT INTO `product_types` VALUES (5,'Vegetables'),(6,'Fruits')");
+
+    $connection->createTable(
+        'products',
+        null,
+        [
+            'columns' => [
+                new Column('id', [
+                    'type'          => Column::TYPE_INTEGER,
+                    'size'          => 10,
+                    'unsigned'      => true,
+                    'notNull'       => true,
+                    'autoIncrement' => true
+                ]),
+                new Column('product_types_id', [
+                    'type'     => Column::TYPE_INTEGER,
+                    'size'     => 10,
+                    'unsigned' => true,
+                    'notNull'  => true
+                ]),
+                new Column('name', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 70,
+                    'notNull' => true
+                ]),
+                new Column('price', [
+                    'type'    => Column::TYPE_DECIMAL,
+                    'size'    => 16,
+                    'scale'   => 2,
+                    'notNull' => true
+                ]),
+                new Column('active', [
+                    'type' => Column::TYPE_CHAR,
+                    'size' => 1
+                ]),
+            ],
+            'indexes' => [
+                new Index('PRIMARY', ['id'], 'PRIMARY')
+            ]
+        ]
+    );
+
+    $connection->execute("INSERT INTO `products` VALUES (1,5,'Artichoke','10.50','Y'),(2,5,'Bell pepper','10.40','Y'),(3,5,'Cauliflower','20.10','Y'),(4,5,'Chinese cabbage','15.50','Y'),(5,5,'Malabar spinach','7.50','Y'),(6,5,'Onion','3.50','Y'),(7,5,'Peanut','4.50','Y')");
+
+    $connection->createTable(
+        'users',
+        null,
+        [
+            'columns' => [
+                new Column('id', [
+                    'type'          => Column::TYPE_INTEGER,
+                    'size'          => 10,
+                    'unsigned'      => true,
+                    'notNull'       => true,
+                    'autoIncrement' => true
+                ]),
+                new Column('username', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 32,
+                    'notNull' => true
+                ]),
+                new Column('password', [
+                    'type'    => Column::TYPE_CHAR,
+                    'size'    => 40,
+                    'notNull' => true
+                ]),
+                new Column('name', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 120,
+                    'notNull' => true
+                ]),
+                new Column('email', [
+                    'type'    => Column::TYPE_VARCHAR,
+                    'size'    => 70,
+                    'notNull' => true
+                ]),
+                new Column('created_at', [
+                    'type'    => Column::TYPE_TIMESTAMP,
+                    'notNull' => true,
+                    'default' => 'CURRENT_TIMESTAMP'
+                ]),
+                new Column('active', [
+                    'type'    => Column::TYPE_CHAR,
+                    'size'    => 1,
+                    'notNull' => true
+                ]),
+            ],
+            'indexes' => [
+                new Index('PRIMARY', ['id'], 'PRIMARY')
+            ]
+        ]
+    );
+
+    $connection->execute("INSERT INTO users VALUES (1,'demo', 'c0bd96dc7ea4ec56741a4e07f6ce98012814d853','Phalcon Demo','demo@phalconphp.com','2012-04-10 20:53:03','Y')");
+
+    $connection->commit();
+
+} catch (\Exception $e) {
+
+    if ($connection->isUnderTransaction()) {
+        $connection->rollback();
+    }
+
+    echo $e->getMessage(), PHP_EOL;
+    echo $e->getTraceAsString(), PHP_EOL;
 }
