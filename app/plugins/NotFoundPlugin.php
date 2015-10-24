b32e702435d9d1571163bb133c17440403186ef4
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-24 19:56:49 +0300

    Fixed PHPDoc

--- app/plugins/NotFoundPlugin.php
+++ app/plugins/NotFoundPlugin.php
@@ -18,7 +18,9 @@ class NotFoundPlugin extends Plugin
 	 * This action is executed before execute any action in the application
 	 *
 	 * @param Event $event
-	 * @param Dispatcher $dispatcher
+	 * @param MvcDispatcher $dispatcher
+	 * @param Exception $exception
+	 * @return boolean
 	 */
 	public function beforeException(Event $event, MvcDispatcher $dispatcher, Exception $exception)
 	{
