41bd622c026b851e25d2a09bf12f0a0bac688ba3
Serghei Iakovlev <sadhooklay@gmail.com>
2015-10-24 16:27:24 +0300

    Cleaned entry point

--- public/index.php
+++ public/index.php
@@ -5,32 +5,28 @@ error_reporting(E_ALL);
 use Phalcon\Mvc\Application;
 use Phalcon\Config\Adapter\Ini as ConfigIni;
 
-//$_GET['_url'] = '/contact/send';
-//$_SERVER['REQUEST_METHOD'] = 'POST';
-
 try {
+    define('APP_PATH', realpath('..') . '/');
 
-	define('APP_PATH', realpath('..') . '/');
+    /**
+     * Read the configuration
+     */
+    $config = new ConfigIni(APP_PATH . 'app/config/config.ini');
 
-	/**
-	 * Read the configuration
-	 */
-	$config = new ConfigIni(APP_PATH . 'app/config/config.ini');
+    /**
+     * Auto-loader configuration
+     */
+    require APP_PATH . 'app/config/loader.php';
 
-	/**
-	 * Auto-loader configuration
-	 */
-	require APP_PATH . 'app/config/loader.php';
+    /**
+     * Load application services
+     */
+    require APP_PATH . 'app/config/services.php';
 
-	/**
-	 * Load application services
-	 */
-	require APP_PATH . 'app/config/services.php';
+    $application = new Application($di);
 
-	$application = new Application($di);
-
-	echo $application->handle()->getContent();
-
+    echo $application->handle()->getContent();
 } catch (Exception $e){
-	echo $e->getMessage();
+    echo $e->getMessage() . '<br>';
+    echo '<pre>' . $e->getTraceAsString() . '</pre>';
 }
