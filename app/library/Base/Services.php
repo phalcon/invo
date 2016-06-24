f56d03c9ca605fd8cc046e689a1ada77e6967189
Tugrul Topuz <tugrultopuz@gmail.com>
2016-06-24 12:13:32 +0300

    Service Container Base Class

---
+++ app/library/Base/Services.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Base;
+
+class Services extends \Phalcon\DI\FactoryDefault
+{
+    public function __construct($config)
+    {
+        parent::__construct();
+        
+        $this->setShared('config', $config);
+        $this->bindServices();
+    }
+    
+    protected function bindServices()
+    {
+        $reflection = new \ReflectionObject($this);
+        $methods = $reflection->getMethods();
+        
+        foreach ($methods as $method) {
+
+            if ((strlen($method->name) > 10) && (strpos($method->name, 'initShared') === 0)) {
+                $this->set(lcfirst(substr($method->name, 10)), $method->getClosure($this));
+                continue;
+            }
+            
+            if ((strlen($method->name) > 4) && (strpos($method->name, 'init') === 0)) {
+                $this->set(lcfirst(substr($method->name, 4)), $method->getClosure($this));
+            }
+
+        }
+        
+    }
+}
