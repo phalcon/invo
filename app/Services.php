7df4a654a9116f369d87263e8ab6adc42c539c61
Tugrul Topuz <tugrultopuz@gmail.com>
2016-06-27 16:48:16 +0300

    Whitespace Correction

--- app/Services.php
+++ app/Services.php
@@ -12,118 +12,120 @@ use Phalcon\Events\Manager as EventsManager;
 
 class Services extends \Base\Services
 {
+
     /**
-    * We register the events manager
-    */
+     * We register the events manager
+     */
     protected function initDispatcher()
     {
         $eventsManager = new EventsManager;
 
-	/**
-	 * Check if the user is allowed to access certain action using the SecurityPlugin
-	 */
-	$eventsManager->attach('dispatch:beforeDispatch', new SecurityPlugin);
+        /**
+         * Check if the user is allowed to access certain action using the SecurityPlugin
+         */
+        $eventsManager->attach('dispatch:beforeDispatch', new SecurityPlugin);
 
-	/**
-	 * Handle exceptions and not-found exceptions using NotFoundPlugin
-	 */
-	$eventsManager->attach('dispatch:beforeException', new NotFoundPlugin);
+        /**
+         * Handle exceptions and not-found exceptions using NotFoundPlugin
+         */
+        $eventsManager->attach('dispatch:beforeException', new NotFoundPlugin);
 
-	$dispatcher = new Dispatcher;
-	$dispatcher->setEventsManager($eventsManager);
+        $dispatcher = new Dispatcher;
+        $dispatcher->setEventsManager($eventsManager);
 
-	return $dispatcher;
+        return $dispatcher;
     }
-    
+
     /**
-    * The URL component is used to generate all kind of urls in the application
-    */
+     * The URL component is used to generate all kind of urls in the application
+     */
     protected function initUrl()
     {
         $url = new UrlProvider();
-	$url->setBaseUri($this->get('config')->application->baseUri);
-	return $url;
+        $url->setBaseUri($this->get('config')->application->baseUri);
+        return $url;
     }
 
     protected function initView()
     {
         $view = new View();
 
-	$view->setViewsDir(APP_PATH . $this->get('config')->application->viewsDir);
+        $view->setViewsDir(APP_PATH . $this->get('config')->application->viewsDir);
 
-	$view->registerEngines(array(
-		".volt" => 'volt'
-	));
+        $view->registerEngines(array(
+            ".volt" => 'volt'
+        ));
 
-	return $view;
+        return $view;
     }
-    
+
     /**
-    * Setting up volt
-    */
+     * Setting up volt
+     */
     protected function initSharedVolt($view, $di)
     {
         $volt = new VoltEngine($view, $di);
 
-	$volt->setOptions(array(
-		"compiledPath" => APP_PATH . "cache/volt/"
-	));
+        $volt->setOptions(array(
+            "compiledPath" => APP_PATH . "cache/volt/"
+        ));
 
-	$compiler = $volt->getCompiler();
-	$compiler->addFunction('is_a', 'is_a');
+        $compiler = $volt->getCompiler();
+        $compiler->addFunction('is_a', 'is_a');
 
-	return $volt;
+        return $volt;
     }
-    
+
     /**
-    * Database connection is created based in the parameters defined in the configuration file
-    */
+     * Database connection is created based in the parameters defined in the configuration file
+     */
     protected function initDb()
     {
         $config = $this->get('config')->get('database')->toArray();
 
-	$dbClass = 'Phalcon\Db\Adapter\Pdo\\' . $config['adapter'];
-	unset($config['adapter']);
+        $dbClass = 'Phalcon\Db\Adapter\Pdo\\' . $config['adapter'];
+        unset($config['adapter']);
 
-	return new $dbClass($config);
+        return new $dbClass($config);
     }
-    
+
     /**
-    * If the configuration specify the use of metadata adapter use it or use memory otherwise
-    */
+     * If the configuration specify the use of metadata adapter use it or use memory otherwise
+     */
     protected function initModelsMetadata()
     {
         return new MetaData();
     }
-    
+
     /**
-    * Start the session the first time some component request the session service
-    */
+     * Start the session the first time some component request the session service
+     */
     protected function initSession()
     {
         $session = new SessionAdapter();
-	$session->start();
-	return $session;
+        $session->start();
+        return $session;
     }
-    
+
     /**
-    * Register the flash service with custom CSS classes
-    */
+     * Register the flash service with custom CSS classes
+     */
     protected function initFlash()
     {
         return new FlashSession(array(
-		'error'   => 'alert alert-danger',
-		'success' => 'alert alert-success',
-		'notice'  => 'alert alert-info',
-		'warning' => 'alert alert-warning'
-	));
+            'error' => 'alert alert-danger',
+            'success' => 'alert alert-success',
+            'notice' => 'alert alert-info',
+            'warning' => 'alert alert-warning'
+        ));
     }
-    
+
     /**
-    * Register a user component
-    */
+     * Register a user component
+     */
     protected function initElements()
     {
         return new Elements();
     }
+
 }
