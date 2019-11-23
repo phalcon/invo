a6aad8d7a44f97e5a98da21a52451e5b6808452e
Anton Vasiliev <aionbase@gmail.com>
2019-11-23 21:44:38 +0000

    #76 - Fix registration of SessionProvider

--- app/providers/SessionProvider.php
+++ app/providers/SessionProvider.php
@@ -5,6 +5,8 @@ namespace Invo\Providers;
 
 use Phalcon\Di\DiInterface;
 use Phalcon\Di\ServiceProviderInterface;
+use Phalcon\Session\Adapter\Stream as SessionAdapter;
+use Phalcon\Session\Manager as SessionManager;
 
 /**
  * Start the session the first time some component request the session service
@@ -14,8 +16,12 @@ final class SessionProvider implements ServiceProviderInterface
     public function register(DiInterface $di)
     {
         $di->setShared('session', function () {
-            $session = new SessionAdapter();
-            $session->start();
+            $session = new SessionManager();
+            $files = new SessionAdapter([
+                'savePath' => sys_get_temp_dir(),
+            ]);
+            $session->setAdapter($files);
+
             return $session;
         });
     }
