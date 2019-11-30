4c9b21e575a5ab4ffeddc400f1756673894ea88d
Anton Vasiliev <aionbase@gmail.com>
2019-11-30 21:02:41 +0000

    #76 - Adapt service registration to the classic format

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Phalcon\Di\FactoryDefault;
+use Phalcon\Di\ServiceProviderInterface;
 use Phalcon\Mvc\Application;
 
 try {
@@ -9,14 +10,21 @@ try {
     require_once $rootPath . '/vendor/autoload.php';
 
     $di = new FactoryDefault();
-    $di->offsetSet('rootPath', $rootPath);
+    $di->offsetSet('rootPath', function() use ($rootPath) {
+        return $rootPath;
+    });
 
     $providers = $rootPath . '/src/config/providers.php';
     if (!file_exists($providers) || !is_readable($providers)) {
         throw new Exception('File providers.php does not exist or is not readable.');
     }
 
-    $di->loadFromPhp($providers);
+    /** @var array $providers */
+    $providers = include_once $providers;
+    /** @var ServiceProviderInterface $provider */
+    foreach ($providers as $provider) {
+        $di->register(new $provider());
+    }
 
     (new Application($di))
         ->handle($_SERVER['REQUEST_URI'])
