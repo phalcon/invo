7f8dd568a9eb5d8f75b891f886b724d72ebeb793
Anton Vasiliev <aionbase@gmail.com>
2019-12-20 21:53:52 +0000

    #76 - Initialize Dotenv

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+use Dotenv\Dotenv;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Mvc\Application;
 
@@ -8,11 +9,22 @@ try {
     $rootPath = realpath('..');
     require_once $rootPath . '/vendor/autoload.php';
 
+    /**
+     * Load ENV variables
+     */
+    Dotenv::createImmutable($rootPath)->load();
+
+    /**
+     * Init Phalcon Dependency Injection
+     */
     $di = new FactoryDefault();
     $di->offsetSet('rootPath', function () use ($rootPath) {
         return $rootPath;
     });
 
+    /**
+     * Register Service Providers
+     */
     $providers = $rootPath . '/config/providers.php';
     if (!file_exists($providers) || !is_readable($providers)) {
         throw new Exception('File providers.php does not exist or is not readable.');
@@ -24,6 +36,9 @@ try {
         $di->register(new $provider());
     }
 
+    /**
+     * Init MVC Application and send output to client
+     */
     (new Application($di))
         ->handle($_SERVER['REQUEST_URI'])
         ->send();
