96f68995721d983cb365b7944bf16654061550dd
Anton Vasiliev <aionbase@gmail.com>
2020-01-12 20:38:37 +0000

    #83 - Move top menu logic into view

--- src/Plugins/ElementsPlugin.php
+++ src/Plugins/ElementsPlugin.php
@@ -24,36 +24,6 @@ class ElementsPlugin extends Injectable
     /**
      * @var array
      */
-    private $headerMenu = [
-        'navbar-left' => [
-            'index' => [
-                'caption' => 'Home',
-                'action' => 'index'
-            ],
-            'invoices' => [
-                'caption' => 'Invoices',
-                'action' => 'index'
-            ],
-            'about' => [
-                'caption' => 'About',
-                'action' => 'index'
-            ],
-            'contact' => [
-                'caption' => 'Contact',
-                'action' => 'index'
-            ],
-        ],
-        'navbar-right' => [
-            'session' => [
-                'caption' => 'Log In/Sign Up',
-                'action' => 'index'
-            ],
-        ]
-    ];
-
-    /**
-     * @var array
-     */
     private $tabs = [
         'Invoices' => [
             'controller' => 'invoices',
@@ -81,45 +51,6 @@ class ElementsPlugin extends Injectable
             'any' => false
         ]
     ];
-
-    /**
-     * Builds header menu with left and right items
-     */
-    public function getMenu(): void
-    {
-        $auth = $this->session->get('auth');
-        if ($auth) {
-            $this->headerMenu['navbar-right']['session'] = [
-                'caption' => 'Log Out',
-                'action' => 'end'
-            ];
-        } else {
-            unset($this->headerMenu['navbar-left']['invoices']);
-        }
-
-        $controllerName = $this->view->getControllerName();
-        foreach ($this->headerMenu as $position => $menu) {
-            echo '<div class="nav-collapse">';
-            echo '<ul class="nav navbar-nav ', $position, '">';
-
-            foreach ($menu as $controller => $option) {
-                if ($controllerName == $controller) {
-                    echo '<li class="active">';
-                } else {
-                    echo '<li>';
-                }
-
-                echo $this->tag->linkTo(
-                    $controller . '/' . $option['action'],
-                    $option['caption']
-                );
-                echo '</li>';
-            }
-
-            echo '</ul>';
-            echo '</div>';
-        }
-    }
 
     /**
      * Returns menu tabs
