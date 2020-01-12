b608978d28f7c58b2fe9b1cae8d7463292e4dd9f
Anton Vasiliev <aionbase@gmail.com>
2020-01-12 21:17:46 +0000

    #83 - Remove ElementsPlugin with its Provider

--- config/providers.php
+++ config/providers.php
@@ -4,7 +4,6 @@ return [
     \Invo\Providers\ConfigProvider::class,
     \Invo\Providers\DatabaseProvider::class,
     \Invo\Providers\DispatcherProvider::class,
-    \Invo\Providers\ElementsProvider::class,
     \Invo\Providers\FlashProvider::class,
     \Invo\Providers\SessionProvider::class,
     \Invo\Providers\SessionBagProvider::class,
