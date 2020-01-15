b8f5b6351a738d53dd79a7af1bd654b7b4c320d2
Anton Vasiliev <aionbase@gmail.com>
2020-01-15 21:14:11 +0000

    #79 - Move testBeforeCreate() test method into functional

--- tests/unit/Models/ContactTest.php
+++ tests/unit/Models/ContactTest.php
@@ -5,7 +5,6 @@ namespace Invo\Tests\Unit\Models;
 
 use Codeception\Test\Unit;
 use Invo\Models\Contact;
-use Phalcon\Db\RawValue;
 use Phalcon\Mvc\Model;
 
 final class ContactTest extends Unit
@@ -15,16 +14,5 @@ final class ContactTest extends Unit
         $class = $this->createMock(Contact::class);
 
         $this->assertInstanceOf(Model::class, $class);
-    }
-
-    public function testBeforeCreate(): void
-    {
-        $class = new Contact();
-        $class->beforeCreate();
-
-        /** @var RawValue $raw */
-        $raw = $class->created_at;
-
-        $this->assertSame('now()', $raw->__toString());
     }
 }
