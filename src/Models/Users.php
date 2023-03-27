8e981cac722b76adae6ead792a47a37d6dbdb708
aircodepl <kontakt@aircode.pl>
2023-03-27 17:01:29 +0200

    Initialize `User` model params

--- src/Models/Users.php
+++ src/Models/Users.php
@@ -13,9 +13,43 @@ declare(strict_types=1);
 
 namespace Invo\Models;
 
+use Phalcon\Db\RawValue;
 use Phalcon\Mvc\Model;
 
 class Users extends Model
 {
+    /**
+     * @var integer|null
+     */
+    public ?int $id = null;
 
+    /**
+     * @var string
+     */
+    public string $username;
+
+    /**
+     * @var string
+     */
+    public string $password;
+
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var string
+     */
+    public string $email;
+
+    /**
+     * @var string|RawValue
+     */
+    public string|RawValue $created_at;
+
+    /**
+     * @var integer
+     */
+    public int $active;
 }
