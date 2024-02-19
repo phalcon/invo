8a19046f287938a5ca4c64fb1ba14980387cb0e0
jjpsos <jjpsos@videotron.ca>
2024-02-19 22:35:57 +0000

    Config Settings

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ use Phalcon\Config\Config;
 return new Config([
     'database' => [
         'adapter'  => $_ENV['DB_ADAPTER'] ?? 'Mysql',
-        'host'     => $_ENV['DB_HOST'] ?? 'localhost',
+        'host'     => $_ENV['DB_HOST'] ?? '127.0.0.1',
         'username' => $_ENV['DB_USERNAME'] ?? 'phalcon',
         'password' => $_ENV['DB_PASSWORD'] ?? 'secret',
         'dbname'   => $_ENV['DB_DBNAME'] ?? 'phalcon_invo',
